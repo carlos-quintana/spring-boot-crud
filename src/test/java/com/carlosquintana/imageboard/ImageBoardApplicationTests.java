bd7b7ec0c23a752200202012bf1bbeab8183b65e
unknown <carlosd.quintana.dev@gmail.com>
2023-03-20 20:59:10 -0500

    Starter project

---
+++ src/test/java/com/carlosquintana/imageboard/ImageBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosquintana.imageboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
